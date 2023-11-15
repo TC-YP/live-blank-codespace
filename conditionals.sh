a75bec0b1d326b7f3f9dceeaf6b07518341c25d4
TC-YP <149639156+TC-YP@users.noreply.github.com>
2023-11-15 02:06:39 +0000

    Modified conditionals

--- conditionals.sh
+++ conditionals.sh
@@ -1,10 +1,12 @@
 name=$1
+admin="TC-YP"
 
-read -p "What is your name? "
+read -p "What is your name? " name
 
-if [[ -z ${name} ]]
-then
+if [[ -z ${name} ]] ; then
     echo "Please enter your name!"
+elif [[ ${name} == ${admin} ]] ; then
+    echo "Welcome back, Pebble"
 else
-    echo "Hi there, $name"
+    echo "Hi there, ${name}"
 fi
