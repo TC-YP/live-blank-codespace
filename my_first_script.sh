009fc56af401ce8a2369aa312aa556cc9bef8179
TC-YP <149639156+TC-YP@users.noreply.github.com>
2023-11-17 02:58:47 +0000

    Added and modified Menu to script

--- my_first_script.sh
+++ my_first_script.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+##
+# Color  Variables
+##
+cyan='\e[36m'
+blue='\e[34m'
+purple='\e[35m'
+clear='\e[0m'
+
+##
+# Color Functions
+##
+
+ColorCyan(){
+	echo -ne $cyan$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
+ColorPurple(){
+	echo -ne $purple$1$clear
+}
+
 #script to return the current server status
 
 server_name=$(hostname)
@@ -45,3 +67,29 @@ function all_checks() {
 
 all_checks
 
+# Menu script
+
+menu(){
+echo -ne "
+$(ColorCyan 'My First Menu')
+$(ColorBlue '1)') Memory usage
+$(ColorBlue '2)') CPU load
+$(ColorBlue '3)') Number of TCP connections 
+$(ColorBlue '4)') Kernel version
+$(ColorBlue '5)') Check All
+$(ColorBlue '0)') Exit
+$(ColorPurple 'Choose an option:') "
+        read a
+        case $a in
+	        1) memory_check ; menu ;;
+	        2) cpu_check ; menu ;;
+	        3) tcp_check ; menu ;;
+	        4) kernel_check ; menu ;;
+	        5) all_checks ; menu ;;
+		0) exit 0 ;;
+		*) echo -e $red"Wrong option."$clear; WrongCommand;;
+        esac
+}
+
+# Call the menu function
+menu
